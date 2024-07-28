8f7594866f55738f596fd94195eacfe3df390005
KTanachit <tanachit2848@gmail.com>
2024-07-28 13:55:21 +0700

    Add files via upload

---
+++ page4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ( ) {
+
+    int a = 3 ;
+    int b = 4 ;
+
+    if( a > b) {
+
+        printf( "The number in A is greater than B. \n  " ) ; 
+        
+    }//end if
+
+    printf( " \n Hello World . ");
+
+    return 0 ;
+}
